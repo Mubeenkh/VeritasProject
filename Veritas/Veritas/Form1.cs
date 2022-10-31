b6c9e2822971564f943fd99f1fa2374696932321
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2022-10-31 15:15:23 -0400

    Add files via upload

---
+++ Veritas/Veritas/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veritas
+{
+    public partial class VeritasForm : Form
+    {
+        public VeritasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VeritasForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
