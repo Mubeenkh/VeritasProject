e4ed866e018b6218a86838f4b2664acb60c9e300
Mubeen Khan <1962558@edu.vaniercollege.qc.ca>
2022-11-01 17:01:15 -0400

    Hello potatos

--- Veritas/Veritas/Program.cs
+++ Veritas/Veritas/Program.cs
@@ -17,6 +17,7 @@ namespace Veritas
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new VeritasForm());
+            Console.WriteLine("Hello potatos");
         }
     }
 }
