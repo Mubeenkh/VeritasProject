23d4c950fb4326149b04950335508083e4e86557
%GoMerKs% <skykings98657@gmail.com>
2022-12-03 17:42:55 -0500

    Gui Tested Changed

---
+++ VeritasProject/VeritasProject/Veritas/Veritas/CategoryForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veritas
+{
+    public partial class CategoryForm : Form
+    {
+        
+        public CategoryForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void ExitCatButton_Click(object sender, EventArgs e)
+        {
+
+            //VeritasForm veritasForm = new VeritasForm();
+            //veritasForm.Show();
+            VeritasForm.Current.Show();
+            this.Close();
+            
+        }
+
+        private void mathButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void historyButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnglishButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void programmingButton_Click(object sender, EventArgs e)
+        {
+            ProgQuestionForm progQuestionForm = new ProgQuestionForm();
+            this.Close();
+            //this.Hide();
+            progQuestionForm.Show();
+
+        }
+
+        private void CategoryForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
---
+++ VeritasProject/VeritasProject/Veritas/Veritas/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veritas
+{
+    public partial class VeritasForm : Form
+    {
+        public static VeritasForm Current {get; set;}
+        SoundPlayer clickSound = new SoundPlayer(@"clickSound.wav");
+        SettingsDialog settingsDialog = new SettingsDialog();
+
+        //Setting up the the Main Page Animation
+        int titleCounter = 0;
+        int titleLength = 0;
+        string titleText;
+
+        public VeritasForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            Current = this;
+            clickSound = new SoundPlayer(@"clickSound.wav");
+        }
+
+        private void VeritasForm_Load(object sender, EventArgs e)
+        {
+            musicPlayer.URL = @"Polish Cow Full Version.wav";
+            musicPlayer.settings.playCount = 9999;
+            musicPlayer.Visible = false;
+            musicPlayer.Ctlcontrols.play();
+            musicPlayer.settings.volume = 50;
+
+            //Title Animation
+             titleText = veritasLabel.Text;
+            titleLength = titleText.Length;
+        }
+
+        private void startButtom_Click(object sender, EventArgs e)
+        {
+            if (settingsDialog.Sound)
+            {
+                clickSound.Play();
+            }
+            CategoryForm categoryForm = new CategoryForm();
+            Current.Hide();
+            categoryForm.Show();
+            
+        }
+
+        private void settingButton_Click(object sender, EventArgs e)
+        {
+            if (settingsDialog.Sound)
+            {
+                clickSound.Play();
+            }
+            settingsDialog.ShowDialog();
+        }
+
+        private void devButton_Click(object sender, EventArgs e)
+        {
+            if (settingsDialog.Sound)
+            {
+                clickSound.Play();
+            }
+
+            var text = "Just Mubeen(me) For Now, nahhh and me too";
+            MessageBox.Show(text, "Developers", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+            
+        }
+
+        private void fullExitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void veritasLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
